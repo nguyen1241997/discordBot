2e8263ef151b85ddf8a351963cab9cbca1b30bf7
Nguyen Vu Nguyen <nguyen1241997@gmail.com>
2021-06-18 20:54:26 +0700

    update

--- bot.py
+++ bot.py
@@ -1,8 +1,6 @@
 import discord
 from discord.ext import commands
 import random
-import json
-import requests
 import os
 
 bot = commands.Bot(command_prefix='.')
@@ -14,56 +12,8 @@ async def on_ready():
     print(bot.user.id)
 
 @bot.command()
-async def de(ctx, n=1):
-    await ctx.channel.purge(limit = n+1)
-
-@bot.command(description='For when you wanna settle the score some other way')
-async def choose(ctx, *choices: str):
-    """Chooses between multiple choices."""
-    await ctx.send(random.choice(choices))
-
-@bot.command()
-async def joined(ctx, member: discord.Member):
-    """Says when a member joined."""
-    await ctx.send('{0.name} joined in {0.joined_at}'.format(member))
-
-def get_quote():
-    response = requests.get("https://zenquotes.io/api/random")
-    json_data = json.loads(response.text)
-    quote = json_data[0]['q'] + " - " + json_data[0]['a']
-    return quote
-
-@bot.command()
-async def quote(ctx):
-    quote = get_quote()
-    await ctx.send(quote)
-
-@bot.command()
-async def meme(ctx):
-    response = requests.get("https://some-random-api.ml/meme")
-    json_data = json.loads(response.text)
-    url = json_data['image']
-    await ctx.send(url)
-
-animals = ['cat', 'dog', 'panda', 'red_panda', 'fox', 'birb', 'koala']
-@bot.command()
-async def ani(ctx):
-    animal = random.choice(animals)
-    url = 'https://some-random-api.ml/img/' + animal
-    response = requests.get(url)
-    json_data = json.loads(response.text)
-    url = json_data['link']
-    await ctx.send(url)
-
-@bot.command()
-async def im(ctx):
-    name = str(random.randint(0,5)) + '.png'
-    await ctx.send(file=discord.File(name))
-
-@bot.command()
-async def img(ctx):
-    url = 'https://picsum.photos/id/' + str(random.randint(0,1000)) + '/1500/1000'
-    await ctx.send(url)
+async def ping(ctx):
+    await ctx.send(f'{round(bot.latency*1000)} ms')
 
 @bot.command()
 async def m(ctx):
